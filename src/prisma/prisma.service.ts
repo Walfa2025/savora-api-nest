508994699dd22a3ae31cd1c8f4ea011d9883f243
William Alfarhat <williamalfa@icloud.com>
2026-01-14 16:09:39 +0100

    fix: use PrismaClient without sqlite adapter

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,10 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
-    const url = process.env.DATABASE_URL || 'file:./dev.db';
-    const adapter = new PrismaBetterSqlite3({ url });
-    super({ adapter, log: ['warn', 'error'] });
+    super({ log: ['warn', 'error'] });
   }
 
   async onModuleInit() {
